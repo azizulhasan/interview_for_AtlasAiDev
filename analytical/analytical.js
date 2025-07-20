8c4db1f4cb6bceb66d7c98c2e844fc5ec760d98b
Shahriar-Azad <shahriar.azad123@gmail.com>
2025-07-20 16:04:46 +0600

    interview

---
+++ analytical/analytical.js
@@ -0,0 +1,28 @@
+
+// Easy level
+
+// answer no-01: the answer would be false cause 0.1 + 0.2 equal would be 0.33333333 
+// which is not strictly equal to 0.3.
+// but if we do math.floor(0.1+0.2) then the strictly equal to 0.3 will true..
+
+
+
+
+// Medium level
+
+// answer no-06:
+
+// the output should be 3 3 3 cause for the setTimeout function it wont be 0,1,2 
+// as it supposed to be happen..but as we used setTimeout function and put 1 second 
+// timeline therefor it wont print 0,1,2
+// instead it would print 3 3 3...
+
+
+// Hard level
+
+// answer no-11:
+// here var a = b = 5;
+// therefore a = 5 and b = 5 as 5 is a integer number therefore 
+// its typeOf b would be Number if we do console.log(typeOf b)
+
+
---
+++ js_react_problem/react.js
@@ -0,0 +1,136 @@
+// easy level ans:02
+
+// function evenOrOdd(num){
+
+//     if (num % 2 == 0){
+//         console.log("Even")
+//     }
+//     else{
+//         console.log("Odd")
+//     }
+// }
+
+// evenOrOdd()
+
+// alternative approach:
+
+// function evenOrOdd(num){
+//     return num % 2 == 0 ? "Even" : "Odd";
+
+// }
+
+// evenOrOdd()
+
+// medium level 
+
+// palindrome word example: racecar
+
+
+// let str = "racecar";
+// let i = 0
+
+// for(i; str.length; i++){
+
+//     if(str[i] === str.length-1){
+//         console.log("Its a palindrome")
+//     }
+//     else{
+//         console.log("Its not a palindrome")
+//     }
+
+
+
+// }    
+
+// let i = 0;
+
+// function counter(){
+//     for(i; i>10; i++){
+//         return 
+//     }
+
+// }
+
+// counter()
+
+
+
+
+
+
+
+
+
+// hard level: ans no: 14
+
+// import react from react;
+
+// import { useState } from react;
+
+// const [count, setCount] = useState(0)
+
+// c = 0
+
+
+// const increaseBtn = document.querySelector('button');
+// let c = 0
+// function increase(){
+
+//     increaseBtn
+
+    
+
+// }
+
+
+// increase()
+
+// function Counter(){
+
+//     return <>
+
+//     <h1>
+//         Counter App
+//     </h1>
+
+//     <div>
+//         <button id="increaseBtn">Increase</button>
+//         <button id="decreaseBtn"> Decrease</button>
+//         <button id="resetBtn">Reset</button>
+//     </div>
+    
+//     </>
+
+// }
+
+
+
+
+// const increaseBtn = document.getElementById('increaseBtn');
+// const decreaseBtn = document.getElementById('decreaseBtn');
+// const resetBtn = document.getElementById('resetBtn');
+
+
+// c = 0
+
+
+// increaseBtn.onclick = ()=>{
+//     increaseBtn.addEventListener("click", ()=>{
+//         c ++;
+//     })
+// }
+
+
+// decreaseBtn.onclick = ()=>{
+//     decreaseBtn.addEventListener("click", ()=>{
+//         c --;
+//     })
+// }
+
+// resetBtn.onclick = ()=>{
+//     resetBtn.addEventListener("click", ()=>{
+//         c = 0;
+//     })
+// }
+
+
